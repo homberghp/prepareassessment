9f28b8f004bf664a282424c83f604ed42eba1bf6
pieter <p.vandenhombergh@fontys.nl>
2016-02-08 17:58:28 +0100

    refactored to have separate header function

--- sql/assessment_score_query3.sql
+++ sql/assessment_score_query3.sql
@@ -1,13 +1,31 @@
 begin work;
-drop function if exists assessment_score_query3(text,char(1));
-create function assessment_score_query3( myevent text, mycategory char(1)) returns text
-as $assessment_score_query3$
-declare
-  th text;
+-- drop function if exists assessment_score_def(text,char(1));
+
+-- drop function if exists assessment_score_query3(text,char(1));
+
+create or replace function assessment_score_def (myevent text, mycategory char(1)) returns text
+as $assessment_score_def$
+declare 
+th text;
 begin
   select array_agg('q'||question||' numeric') into strict th 
   from (select question from assessment_questions where event=myevent and category=mycategory order by question) qq;
-  th := 'crosstab(
+  return 'snummer integer,stick_nr integer,stick_event_repo_id integer,'||regexp_replace(th,'[}"{]','','g');
+end;
+$assessment_score_def$ language 'plpgsql';
+
+create or replace function assessment_score_query3( myevent text, mycategory char(1)) returns text
+as $assessment_score_query3$
+declare
+  th1 text;
+  th2 text;
+begin
+  th1 := assessment_score_def (myevent,mycategory);
+  -- select array_agg('q'||question||' numeric') into strict th 
+  -- from (select question from assessment_questions where event=myevent and category=mycategory order by question) qq;
+  -- raise notice 'th=%',th;
+  
+  th2 := 'crosstab(
   ''select snummer, stick_nr,stick_event_repo_id,question,score 
   from stick_event_repo car 
   join assessment_scores assc using(event,stick_event_repo_id)
@@ -16,10 +34,14 @@ begin
   join student using(snummer)
   where car.event='''''||myevent||''''' and qst.category='''''||mycategory||''''' order by stick_nr,question'',
   ''select question from assessment_questions aqs where aqs.event='''''||myevent||''''' and aqs.category='''''||mycategory||''''' order by question''
-  ) as ct(snummer integer,stick_nr integer,stick_event_repo_id integer,'||regexp_replace(th,'[}"{]','','g')||') ';
-  return th;
+  ) as ct('||th1||') ';
+  return th2;
 end; $assessment_score_query3$ 
 language 'plpgsql';
 \a
-select * from assessment_score_query3('STA120160129','1');
+select * from assessment_score_def('STA120160129','1') ;
+select * from assessment_score_query3('STA120160129','1') ;
+
 commit;
+
+
