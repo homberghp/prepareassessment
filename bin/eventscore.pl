87c3e07920b87c3f73e90ae9c434ef755dcd45b0
Pieter van den Hombergh <p.vandenhombergh@fontys.nl>
2017-02-16 12:19:43 +0100

    first used version

--- bin/eventscore.pl
+++ bin/eventscore.pl
@@ -3,19 +3,24 @@ use strict;
 use DBI;
 use DBD::Pg;
 use DBD::Pg qw(:pg_types);
+use Config::Properties;
+use MIME::Lite;
+# use IO::All;
+use File::Basename;
+use Email::Sender::Simple qw(sendmail);
 
-my ( $row,$snummer, $achternaam, $roepnaam, $stick, $event, $question, $filepath, $weight, $score,$weightedscore);
+my ( $row,$snummer, $achternaam, $roepnaam, $to, $stick, $event, $question, $filepath, $weight, $score,$weightedscore,$email);
 if ($#ARGV < 1) {
-    die qq(Need event and stick\n);
+  die qq(Need event and stick\n);
 }
 
 $event=$ARGV[0];
 $stick=$ARGV[1];
 
-my $dbh= DBI->connect("dbi:Pg:dbname=sebiassessment");#);
+my $dbh= DBI->connect("dbi:Pg:dbname=sebiassessment"); #);
 my ($file,$count);
 $count=0;
-my $query = qq(select snummer, achternaam, roepnaam, stick, event, replace(question,'_','\\_') as question, filepath, weight, score, weight*score as weightedscore
+my $query = qq(select snummer, achternaam, roepnaam, email1 as email,stick, event, replace(question,'_','\\_') as question, filepath, weight, score, weight*score as weightedscore
  from event_scores where event='$event' and stick='$stick');
 
 my $sth = $dbh->prepare($query);
@@ -23,7 +28,7 @@ $sth->execute( );
 $file=$stick.'.tex'; 
 open(TEX,">$file ") or die qq(cannot open $file);
 if ($row = $sth->fetchrow_arrayref) {
- ( $snummer, $achternaam, $roepnaam, $stick, $event, $question, $filepath, $weight, $score,$weightedscore) = @$row;
+  ( $snummer, $achternaam, $roepnaam, $to, $stick, $event, $question, $filepath, $weight, $score,$weightedscore) = @$row;
   print TEX qq(
 \\documentclass[12pt]{article}
 \\usepackage[landscape,scale={0.8,0.8}]{geometry}
@@ -49,11 +54,11 @@ $snummer, $achternaam, $roepnaam, $stick\n
 
 }
 while ($row = $sth->fetchrow_arrayref) {
- ( $snummer, $achternaam, $roepnaam, $stick, $event, $question, $filepath, $weight, $score,$weightedscore) = @$row;
+  ( $snummer, $achternaam, $roepnaam, $to,$stick, $event, $question, $filepath, $weight, $score,$weightedscore) = @$row;
   $count++;
-   if ($count % 2) {
-print TEX qq(\\rowcolor{light-gray});
-}
+  if ($count % 2) {
+    print TEX qq(\\rowcolor{light-gray});
+  }
   print TEX qq($question & $filepath &  $weight &  $score & $weightedscore\\\\\\hline\n);
 }
 
@@ -64,8 +69,8 @@ $sth = $dbh->prepare($query);
 my ( $weightsum, $maxscore, $weightscore, $grade,$gen);
 $sth->execute( );
 while ($row = $sth->fetchrow_arrayref) {
- ( $weightsum, $maxscore, $weightscore, $grade,$gen) = @$row;
-    print TEX qq(
+  ( $weightsum, $maxscore, $weightscore, $grade,$gen) = @$row;
+  print TEX qq(
 & \\textbf{summing up to} & $weightsum & & $weightscore \\\\\\hline
 & Maximum achievable score  & & & $maxscore\\\\\\hline
 \\rowcolor{light-gray}& \\textbf{Preliminary Grade} &&& \\textbf{$grade}\\\\\\hline
@@ -84,7 +89,49 @@ print TEX qq(
 $dbh->disconnect;
 
 close(TEX);
- `pdflatex -interaction=batchmode $stick`;
+`pdflatex -interaction=batchmode $stick`;
+
+my ($message,$subject,$mailbody);
+# $to ='p.vandenhombergh@fontys.nl';
+$mailbody .=qq(
+Dear $roepnaam $achternaam,
+
+Please find attached, the score details of the Java performance assessment $event.
+If you have any questions regarding your result, please contact the teacher of your class.
+Also notice that your personal java1 repository at fontysvenlo.org contains a pristine copy of your
+work.
+
+----
+
+Kind regards, SEBI Venlo Java Teachers.
+Richard van den Ham
+Geert Monsieur
+Pieter van den Hombergh
+
+);
+
+$message = MIME::Lite->new(
+		       From => 'p.vandenhombergh@fontys.nl',
+		       To => $to,
+		       Subject => 'Java 1 Performance assessment score details',
+		       Type => 'multipart/mixed',
+);
+$message->attach(
+		 Type => 'TEXT',
+		 Data => $mailbody
+);
+$message->attach(
+		 Type  => 'applicateio/pdf',
+		 Path  => "$stick.pdf",
+		 Filename => "detailed_score_$stick.pdf",
+		 Disposition => 'attachment',
+);
+
+
+
+$message -> send('sendmail','localhost',Debug=>1);
+
+
 
 exit(0);
 
